d6c5cbfe651dabf969fd9a967bc024c9e99217a2
gab1144 <gabry.rinciari04@gmail.com>
2022-11-17 15:38:57 +0100

    [add] struttura e card

---
+++ src/components/CharacterList.vue
@@ -0,0 +1,35 @@
+<script>
+
+import {store} from '../data/store';
+import CharacterCard from './CharacterCard.vue';
+
+export default {
+  name: 'CharacterList',
+  components:{
+    CharacterCard
+  },
+  data(){
+    return{
+      store
+    }
+  }
+}
+</script>
+
+
+<template>
+
+  <div class="container">
+    <div class="row">
+      <CharacterCard
+        v-for="character of store.charactersListData"
+        :key="character.id"
+        :character="character"/>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+  
+</style>
