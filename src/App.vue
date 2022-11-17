79accebd54ff67eb7fdd376c869d9f8ba3a6bf17
gab1144 <gabry.rinciari04@gmail.com>
2022-11-17 15:39:22 +0100

    [add] struttura e card

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+
+import axios from 'axios';
+import {store} from './data/store';
+import CharacterList from './components/CharacterList.vue';
+
+
+export default {
+  name: 'App',
+  components:{
+    CharacterList
+  },
+  data(){
+    return{
+      store
+    }
+  },
+  methods:{
+    getCharacters(){
+      axios.get(store.apiUrl)
+        .then(result=> {
+          store.charactersListData=result.data;
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
+  },
+  mounted(){
+    this.getCharacters();
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <CharacterList/>
+  </div>
+</template>
+
+
+<style lang="scss">
+  @use './styles/general' as *;
+</style>
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: 'CharacterCard',
+  props:{
+    character: Object
+  }
+}
+</script>
+
+
+<template>
+  <div class="col-3">
+    <div class="custom-card">
+      <div class="img-area">
+        <img :src="character.img" :alt="character.name">
+      </div>
+      <span>{{character.name}}</span>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+  @use '../styles/partials/variables' as *;
+  .custom-card{
+    padding: 15px;
+    margin-bottom: 30px;
+    height: 430px;
+    background-color: $primary-color;
+    img{
+      width: 100%;
+    }
+  }
+</style>
