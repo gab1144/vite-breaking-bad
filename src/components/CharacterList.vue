dcc41b36cab0b315674e5bcef57aaf429af45694
gab1144 <gabry.rinciari04@gmail.com>
2022-11-18 16:57:15 +0100

    [add] bonus

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -3,12 +3,14 @@
 import {store} from '../data/store';
 import CharacterCard from './CharacterCard.vue';
 import AppLoading from './AppLoading.vue';
+import ResultsCounter from './ResultsCounter.vue';
 
 export default {
   name: 'CharacterList',
   components:{
     CharacterCard,
-    AppLoading
+    AppLoading,
+    ResultsCounter
   },
   data(){
     return{
@@ -31,9 +33,7 @@ export default {
 
 
     <div class="row" v-if="store.isLoaded">
-      <div class="col character-number">
-        <span>Found {{store.charactersListData.length}} characters </span>
-      </div>
+      <ResultsCounter/>
     </div>
 
     <div class="row row-cols-5 character-list" v-if="store.isLoaded">
@@ -51,16 +51,6 @@ export default {
   .character-area{
     background-color: white;
     padding: 50px;
-
-    .character-number{
-      background-color: $counter-color;
-      color: white;
-      height: 55px;
-      margin-bottom: 15px;
-      display: flex;
-      align-items: center;
-      font-weight: 800;
-    }
   }
 
 </style>
---
+++ src/components/ResultsCounter.vue
@@ -0,0 +1,37 @@
+<script>
+
+import {store} from '../data/store';
+
+export default {
+  name: 'ResultsCounter',
+  data(){
+    store
+  },
+  computed:{
+    characterNumber(){
+      return store.charactersListData.length;
+    }
+  }
+}
+</script>
+
+
+<template>
+    <div class="col character-number">
+      <span>Found {{characterNumber}} characters </span>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+  @use '../styles/partials/variables' as *;
+  .character-number{
+    background-color: $counter-color;
+    color: white;
+    height: 55px;
+    margin-bottom: 15px;
+    display: flex;
+    align-items: center;
+    font-weight: 800;
+  }
+</style>
